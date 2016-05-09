7e1d6da3dc5c18c6aba103253d82cd32becca419
elflaco68 <sphynxz@gmail.com>
2016-05-09 22:33:20 +0200

    One collect may contains X task

--- inc/collect_file_content.class.php
+++ inc/collect_file_content.class.php
@@ -191,9 +191,7 @@ class PluginFusioninventoryCollect_File_Content extends CommonDBTM {
       echo "<th>".__('Size', 'fusioninventory')."</th>";
       echo "</tr>";
 
-      $a_data = $this->find("`computers_id`='".$computers_id."'",
-                              "`plugin_fusioninventory_collects_files_id`,
-                                 `pathfile`");
+      $a_data = $this->find("`computers_id`='".$computers_id."'");
       foreach ($a_data as $data) {
          echo "<tr class='tab_bg_1'>";
          echo '<td>';
